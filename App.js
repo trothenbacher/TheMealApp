5faa2fe964b9c091473a6c52529cdb34c659026e
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-08-14 20:38:00 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+export default function App() {
+  
+  const [fontLoaded, setFontLoaded] = useState(false); 
+
+  if(!fontLoaded) {
+    return 
+    <AppLoading 
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)} 
+    />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
