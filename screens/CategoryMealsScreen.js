bb9bc6f31527f6a04455654f813b1be8c25ef4d7
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-08-17 10:05:33 +0200

    add favorite icon to MealDetailScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -12,7 +12,14 @@ const CategoryMealsScreen = props => {
                 duration={itemData.item.duration}
                 complexity={itemData.item.complexity}
                 affordability={itemData.item.affordability}
-                onSelectMeal={() => {}}
+                onSelectMeal={() => {
+                    props.navigation.navigate({
+                        routeName: 'MealDetail',
+                        params: {
+                            mealId: itemData.item.id
+                        }
+                    });
+                }}
             />
         );
     };
